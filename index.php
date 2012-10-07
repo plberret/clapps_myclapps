b8480c46b6bc687e2ae2d211960f166a95b6bf15
plberret <pl.berret@gmail.com>
2012-10-07 13:26:59 +0200

    premiere maj

--- index.php
+++ index.php
@@ -25,28 +25,69 @@
 		</nav>
 	</header>
 	<section>
+		
+		<form id="newProject" style="margin: 30px 0; background: #DDD;">
+			<p>
+				<label for="">Nom du projet </label>
+				<input type="text" />
+			</p>
+			<p>
+				<label for="">Description</label>
+				<textarea name="" id="" cols="30" rows="10"></textarea>
+			</p>
+			<div id="profileList">
+				<p>
+					<label for="">Poste recherché : </label>
+					<input type="text" name="profile1" />
+					<select name="domain1" id="domain1">
+						<option value="actor">Acteur</option>
+						<option value="technicien">Technicien</option>
+					</select>
+				</p>
+				<p>
+					<label for="">Poste recherché : </label>
+					<input type="text" name="profile1" />
+					<select name="domain1" id="domain1">
+						<option value="actor">Acteur</option>
+						<option value="technicien">Technicien</option>
+					</select>
+				</p>
+				<a href="#">Ajouter un poste</a>
+			</div>
+			<p>
+				<input type="submit" value="envoyer" />
+			</p>
+		</form>
 	
 		<?php
 			
 			$getProjects=getProjects();
 			
+			echo '<pre>';
+		//	print_r($getProjects);
+			echo '</pre>';
+			
 			foreach ($getProjects as $project) {
 				
-			/*	<article>
+		?>
+		
+				<article style="margin: 30px 0; background: #CCC;">
 					<div class="preview">
-						<h2>Nom du projet</h2>
+						<h2><?php echo $project['Title']; ?></h2>
 						<div>Ajouté par <span>Admin</span> le <span>29 octobre 2012</span></div>
 						<div class="available">
-							<a href="#">Acteurs dispo</a>
-							<a href="#">techniciens dispo</a>
+							<?php $activeActors=getActiveActors($project['ID_project']); ?>
+							<?php $activeTechnicians=getActiveTechnicians($project['ID_project']); ?>
+							<a href="#"><?php echo count($activeActors); ?> Acteurs dispo</a>
+							<a href="#"><?php echo count($activeTechnicians); ?> techniciens dispo</a>
 						</div>
 						<div class="share">
 							<a href="#">Ajouter aux favoris</a>
-							<a href="#">Partager l'annonce'</a>
+							<a href="#">Partager l'annonce</a>
 						</div>
 						<div class="desc">
 							<img src="#" alt="photo profil" />
-							<p>Description</p>
+							<p><?php echo $project['Description']; ?></p>
 						</div>
 						<a href="#">Voir plus</a>
 					</div><!-- fin preview -->
@@ -56,18 +97,15 @@
 						</div>
 						<div class="profiles">
 							<ul>
-								<li>
-									<span>Img</span>
-									<p>Un acteur blond de 30 ans</p>
-									<a href="#">Candidater</a>
-									<a href="#">Candidat trouvé</a>
-								</li>
-								<li>
-									<span>Img</span>
-									<p>Un acteur noir de 40 ans</p>
-									<a href="#">Candidater</a>
-									<a href="#">Candidat trouvé</a>
-								</li>
+								<?php $getProfiles=getProfiles($project['ID_project']); ?>
+								<?php foreach ($getProfiles as $profile) { ?>
+									<li>
+										<span>Img</span>
+										<p><?php echo $profile['Person']; ?></p>
+										<a href="#">Candidater</a>
+										<a href="#">Candidat trouvé</a>
+									</li>
+								<?php } ?>
 							</ul>
 						</div><!-- fin profile -->
 						<div class="manage">
@@ -76,10 +114,8 @@
 						</div>
 					</div><!-- fin more -->
 				</article>
-			   */
-			}
-			
-		 ?>
+				
+		<?php } ?>
 		
 	</section>
 		
