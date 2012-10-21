93e98076ae2dbf1a1987f7b3fab1b8a57b76ddb1
plberret <pl.berret@gmail.com>
2012-10-21 18:55:37 +0200

    inté popin

--- poppin/addProject.php
+++ poppin/addProject.php
@@ -1,32 +1,47 @@
+
 <form id="newProject" action="requests/addProject.php" method="post">
-	<p>
-		<label for="title">Nom du projet </label>
-		<input type="text" name="title" id='title'/>
-	</p>
-	<p>
-		<label for="desc">Description</label>
-		<textarea id="desc"  name="desc" cols="30" rows="10"></textarea>
-	</p>
-	<div id="profileList">
+	
+	<h2>Ajouter une annonce</h2>
+	
+	<div class="desc">
+		<h3>Informations générales</h3>
 		<p>
-			<label for="">Poste recherché : </label>
-			<input type="text" name="profile[]" />
-			<select name="domain[]">
-				<option value="actor">Acteur</option>
-				<option value="technicien">Technicien</option>
-			</select>
+			<input type="text" name="title" id="title" value="Titre de votre annonce" />
+			<em>80 caractères restants</em>
 		</p>
 		<p>
-			<label for="">Poste recherché : </label>
-			<input type="text" name="profile[]" />
-			<select name="domain[]">
-				<option value="actor">Acteur</option>
-				<option value="technicien">Technicien</option>
-			</select>
+			<textarea id="desc" name="desc" >Description de votre annonce...</textarea>
 		</p>
-		<a href="#" id="add-post">Ajouter un poste</a>
 	</div>
-	<p>
-		<input type="submit" id="add-project" value="envoyer" />
+	
+	<div class="profiles">
+		<h3>Ajouter un(des) poste(s)</h3>
+		<p class="clearfix">
+			<input type="text" name="profile[]" class="entitled" value="Intitulé du poste recherché" />
+			<select name="domain[]">
+				<option value="actor">Acteur</option>
+				<option value="technicien">Technicien</option>
+			</select>
+			<a href="#" class="less number_control">-</a>
+			<input type="text" value="7" class="number" />
+			<a href="#" class="more number_control">+</a>
+			<a href="#" class="delete">-</a>
+		</p>
+		<p class="clearfix">
+			<input type="text" name="profile[]" class="entitled" value="Intitulé du poste recherché" />
+			<select name="domain[]">
+				<option value="actor">Acteur</option>
+				<option value="technicien">Technicien</option>
+			</select>
+			<a href="#" class="less number_control">-</a>
+			<input type="text" value="7" class="number" />
+			<a href="#" class="more number_control">+</a>
+			<a href="#" id="add-post">+</a>
+		</p>
+	</div>
+	
+	<p class="clearfix">
+		<input type="submit" id="add-project" value="Publier l'annonce" />
 	</p>
+
 </form>
