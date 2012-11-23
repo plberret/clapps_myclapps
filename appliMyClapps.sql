330510e4490569b8c8b164cde161157431723f7b
Léonard Drouillas <leonard.drouillas@gmail.com>
2012-11-23 13:04:11 +0100

    update db

--- appliMyClapps.sql
+++ appliMyClapps.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.5.9)
 # Database: appliMyClapps
-# Generation Time: 2012-10-23 15:26:59 +0000
+# Generation Time: 2012-11-23 12:03:06 +0000
 # ************************************************************
 
 
@@ -54,6 +54,7 @@ CREATE TABLE `mc_profile` (
   `id_profile` int(10) NOT NULL AUTO_INCREMENT,
   `id_project` int(10) NOT NULL,
   `person` varchar(250) NOT NULL,
+  `occurence` int(11) DEFAULT NULL,
   `domain` int(1) NOT NULL,
   `current_state` int(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id_profile`)
@@ -62,17 +63,42 @@ CREATE TABLE `mc_profile` (
 LOCK TABLES `mc_profile` WRITE;
 /*!40000 ALTER TABLE `mc_profile` DISABLE KEYS */;
 
-INSERT INTO `mc_profile` (`id_profile`, `id_project`, `person`, `domain`, `current_state`)
+INSERT INTO `mc_profile` (`id_profile`, `id_project`, `person`, `occurence`, `domain`, `current_state`)
 VALUES
-	(1,1,'Un acteur blond',1,1),
-	(2,1,'Une maquilleuse',2,1),
-	(3,1,'Un acteur roux',1,0),
-	(9,30,'Acteur 1',1,1),
-	(10,30,'Tecos 1',2,1),
-	(11,30,'Acteur 2',1,1),
-	(12,30,'Tecos 2',2,1),
-	(13,31,'Costumier',2,1),
-	(14,32,'TEst',2,1);
+	(1,1,'Un acteur blond',1,1,1),
+	(2,1,'Une maquilleuse',2,2,1),
+	(3,1,'Un acteur roux',1,1,0),
+	(9,30,'Acteur 1',1,1,1),
+	(10,30,'Tecos 1',1,2,1),
+	(11,30,'Acteur 2',1,1,1),
+	(12,30,'Tecos 2',1,2,1),
+	(13,31,'Costumier',3,2,1),
+	(14,32,'TEst',1,2,1),
+	(15,33,'Payday',4,1,1),
+	(16,34,'PORNOOO',1,1,1),
+	(17,34,'PORNOOOO',12,1,1),
+	(18,35,'En harley',1,1,1),
+	(19,35,'Davinson',6,2,1),
+	(20,36,'Yogourt',3,1,1),
+	(21,37,'Actif',1,1,1),
+	(22,38,'Moi',1,1,1),
+	(23,39,'Toi',1,1,1),
+	(24,40,'aze',1,1,1),
+	(25,41,'azedsq',1,1,1),
+	(26,42,'2',2,1,1),
+	(27,43,'fe',1,1,1),
+	(28,44,'Du fruit',1,1,1),
+	(29,44,'De l\\\'eau de source',1,1,1),
+	(30,44,'Du fun',1,2,1),
+	(31,45,'test',1,2,1),
+	(32,46,'test',1,1,1),
+	(33,47,'test',1,1,1),
+	(34,48,'Blonde 90 D',7,1,1),
+	(35,48,'Doubleur',1,1,1),
+	(36,48,'Maquilleuse',14,2,1),
+	(37,48,'Prépareuse',1,2,1),
+	(38,48,'Cadreur',18,2,1),
+	(39,49,'Calor',1,1,1);
 
 /*!40000 ALTER TABLE `mc_profile` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -104,8 +130,23 @@ VALUES
 	(4,'Nouveau films 18','Description 18',2,0,'0000-00-00'),
 	(5,'Nouveau films 21','Description 21',2,2,'0000-00-00'),
 	(30,'MyTest','',1,1,'2012-10-21'),
-	(31,'Court métrage pornographique','Un super film pornographique animalier',1,1,'2012-10-22'),
-	(32,'TEst','Test',1,1,'2012-10-22');
+	(32,'TEst','Test',1,1,'2012-10-22'),
+	(33,'Payday','Description de Payday',1,1,'2012-10-23'),
+	(34,'Un film porno','Porno',1,1,'2012-10-25'),
+	(35,'11em annonce','Besoin de rien ni personne',1,1,'2012-11-16'),
+	(36,'Test','Tesst',1,1,'2012-11-16'),
+	(37,'Teeest','2',1,1,'2012-11-16'),
+	(38,'Test','3',1,1,'2012-11-16'),
+	(39,'Test','4',1,1,'2012-11-16'),
+	(40,'Test','5',1,1,'2012-11-16'),
+	(41,'test','6',1,1,'2012-11-16'),
+	(42,'Yéyéyé',':D',1,1,'2012-11-16'),
+	(43,'(','zer',1,1,'2012-11-16'),
+	(44,'Pierre-Loic','Est trop balaise',1,1,'2012-11-16'),
+	(45,'Test','testes',1,1,'2012-11-16'),
+	(46,'test','test',1,1,'2012-11-16'),
+	(47,'Test','tesy',1,1,'2012-11-16'),
+	(49,'Calor','Calor',1,1,'2012-11-23');
 
 /*!40000 ALTER TABLE `mc_project` ENABLE KEYS */;
 UNLOCK TABLES;
