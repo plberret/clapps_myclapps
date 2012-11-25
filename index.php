5f68ac565e2314181e4b957c80105804c904b4ed
Léonard Drouillas <leonard.drouillas@gmail.com>
2012-11-25 13:21:19 +0100

    script on index page with "echo"

--- index.php
+++ index.php
@@ -157,6 +157,13 @@
 			cookie : true, // enable cookies to allow the server to access the session
 			xfbml : true // parse XFBML
 		});
+		FB.Canvas.setAutoGrow();
+		(function() {
+		    var e = document.createElement("script");
+		    e.src = document.location.protocol + "//connect.facebook.net/en_US/all.js";
+		    e.async = true;
+		    document.getElementById("fb-root").appendChild(e);
+		  }());
 	</script>
 ';?>
 </body>
