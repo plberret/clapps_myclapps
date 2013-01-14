3e2ff3cfba99d3fd9aca0afe0070e343b035f228
plberret <pl.berret@gmail.com>
2013-01-15 00:28:02 +0100

    merge ?

---
+++ api/mailchimp/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+    //API Key - see http://admin.mailchimp.com/account/api
+    $apikey = 'b9ff97cf6c71b2f0519e5dc1b4d6d24c-us6';
+    
+    // A List Id to run examples against. use lists() to view all
+    // Also, login to MC account, go to List, then List Tools, and look for the List ID entry
+    $listId = '3e0f3c74c2';
+
+    //just used in xml-rpc examples
+    $apiUrl = 'http://api.mailchimp.com/1.3/';
+    
+?>
