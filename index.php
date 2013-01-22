65c77a48fcc46c867c49762ada22949960fb82d3
plberret <pl.berret@gmail.com>
2013-01-22 18:45:08 +0100

    fix video

--- index.php
+++ index.php
@@ -468,8 +468,9 @@
 					<div class="display">
 						<div class="mask">
 							<!-- width="681" height="288" -->
-							<video  id="vid" width="690" autoplay autobuffer loop >
-								<source src="./assets/tuto_clapps.f4v" type='video/mp4"'>
+							<video  id="vid" width="690" autoplay autobuffer loop id="my_video_1" class="video-js vjs-default-skin"
+							 src="./assets/tuto_clapps.f4v">
+							
 							</video>
 						</div>
 					</div>
